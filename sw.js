dc4d72599873ea71f3dc350c2779f9ec20714ac3
evona0203 <evonaservice@gmail.com>
2026-04-08 22:18:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// Agenda Evona — Service Worker v7
+// Agenda Evona — Service Worker v8
 // ⚠️  Incrémente ce numéro à chaque déploiement pour vider le cache
-const CACHE_NAME = 'evona-cache-v7';
+const CACHE_NAME = 'evona-cache-v8';
 
 const PRECACHE = [
   './index.html',
